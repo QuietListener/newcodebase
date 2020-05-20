f9f3d489de1de31d4638b277c98b81dd1ce6ade6
junjun <junjun@baicizhan.com>
2020-05-20 15:54:07 +0800

    fx

---
+++ javaBasic/src/main/java/andy/com/javaBasic/DateTest.java
@@ -0,0 +1,26 @@
+package andy.com.javaBasic;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTest {
+
+    @Test
+    public void testCalendar() {
+
+        long nowMs = System.currentTimeMillis();
+        long span = 24 * 60 * 60 * 1000;
+        for (int i = 0; i < 7; i++) {
+            Date d = new Date(nowMs + i * span);
+            System.out.println(new SimpleDateFormat("yyyyMMdd").format(d));
+            Calendar c  = Calendar.getInstance();
+            c.setTime(d);
+            System.out.println(c.get(Calendar.DAY_OF_WEEK));
+
+            System.out.println("---------");
+        }
+    }
+}
