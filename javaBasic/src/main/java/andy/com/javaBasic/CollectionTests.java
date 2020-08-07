0e9a4a38ed6c5276ddf97df5e731f65d32ce9852
junjun <junjun@baicizhan.com>
2020-08-07 10:38:24 +0800

    fx

--- javaBasic/src/main/java/andy/com/javaBasic/CollectionTests.java
+++ javaBasic/src/main/java/andy/com/javaBasic/CollectionTests.java
@@ -2,8 +2,10 @@ package andy.com.javaBasic;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class CollectionTests {
 
@@ -12,5 +14,14 @@ public class CollectionTests {
         int max = Collections.max(Arrays.asList(1,2,-1));
         System.out.println(""+max);
         assert max == 2;
+    }
+
+    @Test
+    public void testRetain(){
+        List<Long> l1 = new ArrayList<>(Arrays.asList(1l,2l));
+        List<Long> l2 =  new ArrayList<>(Arrays.asList(1l,3l));
+        System.out.println(l1.retainAll(l2));
+        System.out.println(l1);
+
     }
 }
