dd1c02416edb4179ec4b16f96b76c7c15e7c49b0
junjun <yangtingjun1@gmail.com>
2020-11-21 14:27:05 +0800

    MySemaphore Ok

--- javaBasic/src/main/java/andy/com/javaConcurrent/aqs/MySemaphore.java
+++ javaBasic/src/main/java/andy/com/javaConcurrent/aqs/MySemaphore.java
@@ -1,16 +1,24 @@
 package andy.com.javaConcurrent.aqs;
 
+import java.util.Date;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class MySemaphore {
 
     private volatile Sync sync = null;
-    public MySemaphore(int count){
+
+    public MySemaphore(int count) {
         sync = new Sync(count);
     }
 
-    public boolean tryAcquire(int c){
-        return true;
+    public void acquire() {
+        sync.acquireShared(1);
+    }
+
+    public void release() {
+        sync.releaseShared(1);
     }
 
     private static class Sync extends AbstractQueuedSynchronizer {
@@ -21,9 +29,9 @@ public class MySemaphore {
 
         @Override
         protected boolean tryReleaseShared(int arg) {
-            while(true){
+            while (true) {
                 int current_count = this.getState();
-                if(compareAndSetState(current_count,current_count+arg)){
+                if (compareAndSetState(current_count, current_count + arg)) {
                     return true;
                 }
             }
@@ -32,16 +40,62 @@ public class MySemaphore {
 
         @Override
         protected int tryAcquireShared(int arg) {
-            while(true) {
+            while (true) {
                 int current_count = this.getState();
                 int remains = current_count - arg;
-                if (remains <= 0) {
+                if (remains < 0) {
                     return remains;
                 }
-                boolean ret = this.compareAndSetState(current_count, remains);
-                if(ret)
-                return remains;
+                if (this.compareAndSetState(current_count, remains)) {
+                    return remains;
+                }
             }
+
         }
+    }
+
+
+    public static void main(String[] args) {
+        MySemaphore s = new MySemaphore(2);
+
+        //Semaphore s = new Semaphore(2);
+        for (int i = 0; i < 4; i++) {
+
+            final int k = i;
+            Thread t = new Thread() {
+
+                @Override
+                public void run() {
+
+                    try {
+                        s.acquire();
+                        System.out.println(getName() + ":" + new Date().getTime() + ": acquire a permit");
+                        try {
+
+                            System.out.println(getName() + ":" + new Date().getTime() + ": doing a job");
+                            TimeUnit.SECONDS.sleep(5);
+                            System.out.println(getName() + ":" + new Date().getTime() + ": finished a job");
+                        } finally {
+                            s.release();
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            t.setName("t-" + i);
+            t.start();
+        }
+
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("结束");
+
     }
 }
