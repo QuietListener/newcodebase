209d34d74b5821ee0f25f6d4c7e760280cd82483
junjun <yangtingjun1@gmail.com>
2021-02-08 18:05:07 +0800

    fx

--- algorithm1/src/main/java/andy/com/algorithm1/beautify_program/part1/Q1_3.java
+++ algorithm1/src/main/java/andy/com/algorithm1/beautify_program/part1/Q1_3.java
@@ -8,8 +8,43 @@ import andy.com.algorithm1.utils.AlgorithmUtils;
 public class Q1_3 {
 
     public static void main(String [] args){
-            int [] pies = new int[]{1,2,3,4};
-             AlgorithmUtils.print(flip(pies,0,pies.length-2));
+
+            int [] pies = new int[]{1,3,4,7,9,2};
+            int count = 0;
+            for(int i = pies.length - 1; i>=0 ;i--){
+                int maxIdx = maxIdx(pies,0,i);
+
+                if(ok(pies,0,i)){
+                    break;
+                }
+                flip(pies,0,maxIdx);
+                flip(pies,0,i);
+                count+=2;
+            }
+
+            AlgorithmUtils.print(pies);
+            System.out.println("count = "+count);
+    }
+
+    public static int maxIdx(int [] pies,int start, int end){
+        int max = start;
+        for(int i = start; i <= end; i++){
+            if(pies[i]>pies[max]){
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    public static boolean ok(int [] pies,int start, int end ){
+        for(int i = start; i <= end-1; i++){
+            if(pies[i] > pies[i+1]){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static int [] flip(int [] pies, int start, int end){
