86e5110b46c5f27991c2ab8c051e29f242b4fe9d
junjun <yangtingjun1@gmail.com>
2021-03-01 19:52:21 +0800

    fx

--- algorithm/src/main/java/andy/com/algorighm/leetcode/slipWindow/Q11.java
+++ algorithm/src/main/java/andy/com/algorighm/leetcode/slipWindow/Q11.java
@@ -2,33 +2,21 @@ package andy.com.algorighm.leetcode.slipWindow;
 
 public class Q11 {
     public int maxArea(int[] height) {
-        int ret = 0;
-        for(int i = 0; i< height.length-1;i++){
-            for(int j = i+1; j < height.length;j++){
-                int s = height[i];
-                int e = height[j];
-                int area = (j-i)*Math.min(s,e);
-                if(ret < area){
-                    ret = area;
-                }
-            }
-        }
 
-        return ret;
     }
 
     public int maxArea1(int[] height) {
         int i = 0;
-        int j = height.length;
+        int j = height.length-1;
         int ret = 0;
 
-        while(i<j){
+        while(i<j && j< height.length){
             int curArea = (j-i)*Math.min(height[i],height[j]);
             if(ret < curArea){
                 ret = curArea;
             }
 
-            if(height[i]>height[j]){
+            if(height[i]<height[j]){
                 i++;
             }else{
                 j--;
