7943665d7a5a0942d8b0b126f09f1fe7fb597144
junjun <yangtingjun1@gmail.com>
2021-03-03 11:21:50 +0800

    fx

---
+++ algorithm1/src/main/java/andy/com/algorithm1/leetcode/Q141.java
@@ -0,0 +1,41 @@
+package andy.com.algorithm1.leetcode;
+
+
+import org.junit.Test;
+
+ class ListNode {
+     int val;
+    ListNode next;
+     ListNode(int x) {
+         val = x;
+        next = null;
+     }
+ }
+
+
+public class Q141 {
+
+    public boolean hasCycle(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+        boolean flag = false;
+        while(slow != null && head != null){
+
+            //快指针追上了慢指针 有环
+            if(slow == fast && flag == true){
+                return true;
+            }
+
+            slow = slow.next;
+            if(fast!=null && fast.next != null){
+                fast = fast.next.next;
+            }else{
+                return false;
+            }
+            flag = true;
+        }
+
+        return false;
+    }
+}
