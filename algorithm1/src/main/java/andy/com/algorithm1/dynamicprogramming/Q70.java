ed94f4e9f4ec81db1c6c0f58e5887bdcef7e6107
junjun <yangtingjun1@gmail.com>
2021-03-05 09:35:51 +0800

    fx

--- algorithm1/src/main/java/andy/com/algorithm1/dynamicprogramming/Q70.java
+++ algorithm1/src/main/java/andy/com/algorithm1/dynamicprogramming/Q70.java
@@ -4,14 +4,13 @@ import org.junit.Test;
 
 public class Q70 {
     public static int  solution(int n){
-
         int []dp = new int[n+1];
         dp[1] = 1;
-        dp[2] = 2;
+        dp[2]= 2;
 
         for(int i = 3; i<=n; i++){
-            int count1 = dp[i-1]
-            int count2 = dp[i-2];
+            int count1 = dp[i-1] ;
+            int count2 = dp[i-2] ;
             dp[i] = count1+count2;
         }
 
